7aacc4798325ccc1d9b58f850122fa9e7453886c
lllll <dovanliem09102014@gmail.com>
2019-06-21 20:28:26 -1000

    Chỉnh id thành String

--- dao/src/main/java/repository/FireForestEntityRepository.java
+++ dao/src/main/java/repository/FireForestEntityRepository.java
@@ -4,5 +4,5 @@ import entity.FireForestEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface FireForestEntityRepository extends PagingAndSortingRepository<FireForestEntity, Long> {
+public interface FireForestEntityRepository extends PagingAndSortingRepository<FireForestEntity, String> {
 }
