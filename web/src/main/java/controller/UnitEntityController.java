390af4970d74d18e3952074def2d79b41e469ed0
lllll <dovanliem09102014@gmail.com>
2019-06-26 15:46:50 -1000

    Đổi tên entity unlawful thành offence

--- web/src/main/java/controller/UnitEntityController.java
+++ web/src/main/java/controller/UnitEntityController.java
@@ -1,7 +1,15 @@
 package controller;
 
+import entity.OffenceEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import service.OffenceEntityManager;
+
+import java.util.Optional;
 
 @Controller
 public class UnitEntityController {
