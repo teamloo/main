33463ec94a05503df2f61312f021039cd0823e8f
maitansang <maitansang.200599@gmail.com>
2019-06-28 19:28:19 +0700

    Sang upload configure

--- web/src/test/java/jungle/controller/DepartmentEntityControllerTest.java
+++ web/src/test/java/jungle/controller/DepartmentEntityControllerTest.java
@@ -1,4 +1,4 @@
-package controller;
+package jungle.controller;
 
 import entity.DepartmentEntity;
 import org.junit.Before;
@@ -11,8 +11,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.ModelAndView;
-
-import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class DepartmentEntityControllerTest {
